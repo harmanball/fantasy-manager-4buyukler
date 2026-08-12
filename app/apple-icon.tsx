a26033f94c5286bb4bd4f5206e6dbb90a07d4910
Selim <harmanballoyna@gmail.com>
2026-08-12 17:46:07 +0300

    Resize Apple icon to 180x180 and rename function

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,12 +1,12 @@
 import { ImageResponse } from "next/og";
 
-export const size = { width: 32, height: 32 };
+export const size = { width: 180, height: 180 };
 export const contentType = "image/png";
 
-export default function Icon() {
+export default function AppleIcon() {
   return new ImageResponse(
     (
-      <svg width="32" height="32" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <svg width="180" height="180" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <rect x="4" y="4" width="92" height="92" rx="10" fill="#0F3D2E" />
         <rect x="12" y="12" width="76" height="76" fill="none" stroke="#F5F1E8" strokeWidth="3" />
         <line x1="12" y1="50" x2="88" y2="50" stroke="#F5F1E8" strokeWidth="3" />
